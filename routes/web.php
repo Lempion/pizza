a29236b5297b205d66a553d5fdd1f08d87d54398
Vitaly <vit.fedor2012@mail.ru>
2024-08-31 15:53:13 +0300

    Подготовил Login для админа

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Http\Controllers\Admin\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get('login', [LoginController::class, 'login'])->name('admin.login');
+    Route::post('login', [LoginController::class, 'authorize'])->name('admin.authorize');
+
+    Route::middleware('admin')->group(function () {
+
+    });
+});
 
 require __DIR__ . '/auth.php';
